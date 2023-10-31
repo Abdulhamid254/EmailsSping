73a5b8d55a28f42bcb2f1ba76be891ee029b6c49
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-10-31 05:01:05 -0700

    Implemented sending email on signup with verification link

--- src/main/java/io/abdul/userserviceemails/service/Implementation/EmailServiceImplementation.java
+++ src/main/java/io/abdul/userserviceemails/service/Implementation/EmailServiceImplementation.java
@@ -1,11 +1,15 @@
 package io.abdul.userserviceemails.service.Implementation;
 
 import io.abdul.userserviceemails.service.EmailService;
+import io.abdul.userserviceemails.utils.EmailUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import static io.abdul.userserviceemails.utils.EmailUtils.getEmailMessage;
 
 @Service
 @RequiredArgsConstructor
@@ -20,13 +24,15 @@ public class EmailServiceImplementation implements EmailService {
     private  final JavaMailSender emailSender;
 
     @Override
+    @Async   // enables the methods to run on a separate thread so that we do not have to wait that long before an email is sent
     public void sendSimpleMailMessage(String name, String to, String token) {
        try {
            SimpleMailMessage message = new SimpleMailMessage();
            message.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
            message.setFrom(fromEmail);
            message.setTo(to);
-           message.setText("Hey this is working!!");
+//           message.setText(EmailUtils.getEmailMessage(name,host,token));
+           message.setText(getEmailMessage(name, host, token));
            emailSender.send(message);
        }catch(Exception exception) {
            System.out.println(exception.getMessage());
@@ -36,21 +42,25 @@ public class EmailServiceImplementation implements EmailService {
     }
 
     @Override
+    @Async
     public void sendMimeMessageWithAttachments(String name, String to, String token) {
 
     }
 
     @Override
+    @Async
     public void sendMimeMessageWithEmbeddedFiles(String name, String to, String token) {
 
     }
 
     @Override
+    @Async
     public void sendHtmlEmail(String name, String to, String token) {
 
     }
 
     @Override
+    @Async
     public void sendHtmlEmailWithEmbeddedFiles(String name, String to, String token) {
 
     }
