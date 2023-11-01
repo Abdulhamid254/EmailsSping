881f64629f879155f718593794c809f57149517d
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-11-01 00:40:58 -0700

    Implemented sending  HTML email with embedded files

--- src/main/java/io/abdul/userserviceemails/service/Implementation/EmailServiceImplementation.java
+++ src/main/java/io/abdul/userserviceemails/service/Implementation/EmailServiceImplementation.java
@@ -36,6 +36,7 @@ public class EmailServiceImplementation implements EmailService {
     public static final String NEW_USER_ACCOUNT_VERIFICATION = "New User Account Verification";
     public static final String UTF_8_ENCODING = "UTF-8";
     private static final String EMAIL_TEMPLATE = "emailtemplate" ;
+    public static final String TEXT_HTML_ENCONDING = "text/html";
     private final JavaMailSender emailSender;
     private final TemplateEngine templateEngine;
 
@@ -152,36 +153,38 @@ public class EmailServiceImplementation implements EmailService {
     @Override
     @Async
     public void sendHtmlEmailWithEmbeddedFiles(String name, String to, String token) {
-//        try {
-//            MimeMessage message = getMimeMessage();
-//            MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8_ENCODING);
-//            helper.setPriority(1);
-//            helper.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
-//            helper.setFrom(fromEmail);
-//            helper.setTo(to);
-//            //helper.setText(text, true);
-//            Context context = new Context();
-//            context.setVariables(Map.of("name", name, "url", getVerificationUrl(host, token)));
-//            String text = templateEngine.process(EMAIL_TEMPLATE, context);
-//
-//            // Add HTML email body
-//            MimeMultipart mimeMultipart = new MimeMultipart("related");
-//            BodyPart messageBodyPart = new MimeBodyPart();
-//            messageBodyPart.setContent(text, TEXT_HTML_ENCONDING);
-//            mimeMultipart.addBodyPart(messageBodyPart);
-//
-//            // Add images to the email body
-//            BodyPart imageBodyPart = new MimeBodyPart();
-//            DataSource dataSource = new FileDataSource(System.getProperty("user.home") + "/Downloads/images/dog.jpg");
-//            imageBodyPart.setDataHandler(new DataHandler(dataSource));
-//            imageBodyPart.setHeader("Content-ID", "image");
-//            mimeMultipart.addBodyPart(imageBodyPart);
-//
-//            emailSender.send(message);
-//        } catch (Exception exception) {
-//            System.out.println(exception.getMessage());
-//            throw new RuntimeException(exception.getMessage());
-//        }
+        try {
+            MimeMessage message = getMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8_ENCODING);
+            helper.setPriority(1);
+            helper.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
+            helper.setFrom(fromEmail);
+            helper.setTo(to);
+            //helper.setText(text, true);
+            Context context = new Context();
+            context.setVariables(Map.of("name", name, "url", getVerificationUrl(host, token)));
+            String text = templateEngine.process(EMAIL_TEMPLATE, context);
+
+            // Add HTML email body
+            MimeMultipart mimeMultipart = new MimeMultipart("related");
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(text, TEXT_HTML_ENCONDING);
+            mimeMultipart.addBodyPart(messageBodyPart);
+
+            // Add images to the email body
+            BodyPart imageBodyPart = new MimeBodyPart();
+            DataSource dataSource = new FileDataSource(System.getProperty("user.home") + "/Downloads/server3.png");
+            imageBodyPart.setDataHandler(new DataHandler(dataSource));
+            imageBodyPart.setHeader("Content-ID", "image");
+            mimeMultipart.addBodyPart(imageBodyPart);
+
+            message.setContent(mimeMultipart);
+
+            emailSender.send(message);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            throw new RuntimeException(exception.getMessage());
+        }
 
     }
 
